707d878b0129aa52f855776e475fbfe3764a9638
Kneelawk <jedidiah.pommert@gmail.com>
2017-03-19 05:03:20 -0700

    Add launcher content and main

--- src/launcher/java/org/kneelawk/unitcirclething/UnitCircleThingLuncher.java
+++ src/launcher/java/org/kneelawk/unitcirclething/UnitCircleThingLuncher.java
@@ -1,7 +1,21 @@
 package org.kneelawk.unitcirclething;
 
+import java.io.IOException;
+
+import org.kneelawk.cpcontrol.CPControl3;
+
 public class UnitCircleThingLuncher {
 	public static void main(String[] args) {
-		
+		CPControl3 cp = new CPControl3("org.kneelawk.unitcirclething.UnitCircleThing");
+
+		cp.addExtractingFromFileLibrary(CPControl3.ME)
+				.addLibrary("application", new CPControl3.DirectoryEntryFilter("app"), CPControl3.ALWAYS_DELETE)
+				.addLibrary("libraries", new CPControl3.DirectoryEntryFilter("libs"), CPControl3.ALWAYS_DELETE);
+
+		try {
+			cp.launch(args);
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/main/java/org/kneelawk/unitcirclething/UnitCircleThing.java
@@ -0,0 +1,9 @@
+package org.kneelawk.unitcirclething;
+
+public class UnitCircleThing {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
